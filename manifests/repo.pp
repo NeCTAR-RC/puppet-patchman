eeb83048e5970b98d00259bdc6f22b1bc60ac133
Marcus Furlong <furlongm@gmail.com>
2015-02-25 11:09:20 +1100

    use upstream repo for installation
    Change-Id: I81ff9cd63e2e18c169c6cc78f3fc16bd9b880ffd

---
+++ manifests/repo.pp
@@ -0,0 +1,24 @@
+# patchman repo class, needs to be in a stage before client class
+
+class patchman::repo {
+
+  if $::http_proxy and $::rfc1918_gateway == 'true' {
+    $key_options = "http-proxy=${::http_proxy}"
+  } else {
+    $key_options = false
+  }
+
+  apt::key { 'patchman':
+    key         => '0412F522',
+    key_server  => 'pgp.mit.edu',
+    key_options => $key_options,
+  }
+
+  apt::source { 'patchman':
+    location    => 'http://repo.openbytes.ie/ubuntu',
+    release     => 'trusty',
+    repos       => 'main',
+    include_src => false,
+  }
+}
+
