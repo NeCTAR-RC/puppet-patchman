7a1def3232691435d900bf329553592465a3eb05
Marcus Furlong <furlongm@gmail.com>
2015-02-25 11:09:20 +1100

    use upstream repo for installation
    Change-Id: I81ff9cd63e2e18c169c6cc78f3fc16bd9b880ffd

---
+++ manifests/client.pp
@@ -0,0 +1,44 @@
+# client class for patchman, installs cronjob and package
+
+class patchman::client($server, $port) {
+
+  $dependencies = ['update-notifier-common', 'curl']
+
+  package { $dependencies:
+    ensure => installed,
+  }
+
+  file { '/etc/patchman':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
+
+  file { '/etc/patchman/conf.d':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0750',
+  }
+
+  package { 'patchman-client':
+    ensure => installed,
+  }
+
+  cron { 'patchman':
+    ensure  => present,
+    command => "/bin/sleep $((RANDOM\\%600)); PATH=/bin:/sbin:/usr/bin:/usr/sbin /usr/local/sbin/patchman-client",
+    user    => 'root',
+    minute  => '00',
+    hour    => '07',
+  }
+
+  file { '/etc/patchman/patchman-client.conf':
+    content => template('patchman/patchman-client.conf.erb'),
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    require => File['/etc/patchman'],
+  }
+}
